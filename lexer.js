3e5625d4e220d78a29d72fae0b5f1e54a28f29c8
Garfield <garfield.chern@foxmail.com>
2025-11-07 03:55:34 +0000

    添加 获取数字/连续数字 分支

--- lexer.js
+++ lexer.js
@@ -5,8 +5,19 @@ function lexer(str) {
         // 1. 排除空格
         if (str[i] === ' ') { i++; continue;}
     
+        // 2. 获取数字 / 连续数字 
+        if (str[i] >= '0' && str[i] <= '9'){
+            let numStr = '';
+            while (i < str.length && str[i] >= '0' && str[i] <= '9'){
+                numStr =+ str[i];
+                i++
+            }
+            console.log(numStr);
+            continue;
+        }
+
         console.log(str[i]);
         i++;
     }
 }
-lexer('5 + 7');
+lexer('let n = 5 + 7');
