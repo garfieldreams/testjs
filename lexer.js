eb9574e97ebc10d906ab5690daa171a064e8ee1c
Garfield <garfield.chern@foxmail.com>
2025-11-14 08:23:41 +0000

    update

--- lexer.js
+++ lexer.js
@@ -2,20 +2,27 @@ function lexer(str) {
     let i = 0;
     const tokens = [];
 
-    // 多字符运算符查找表
-    const operators = new Set([
-        '===', '!==', '==', '!=', '++', '--', '+=', '-=', '*=', '/=', '%=',
-        '&&', '||', '&=', '|=', '<<', '>>', '<=', '>=', '=>', '->',
-        '=', '!', '+', '-', '*', '/', '%', '&', '|', '<', '>'
-    ]);
-
-    whileLoop: while (i < str.length) {
+    /* 主要解析逻辑 
+    1. 排除空格
+    2.0 获取每个数字
+    2.1 获取连续数字
+    3.0 获取每个字母
+    3.1 获取连续字母
+    */
+    while (i < str.length) {
         const char = str[i];
 
         // 1. 排除空格
         if (char === ' ') { i++; continue; }
 
-        // 2. 获取数字 / 连续数字 
+        // 2.0 获取每个数字
+        if (char >= '0' && char <= '9') {
+            tokens.push({ type: 'Number', value: char });
+            i++;
+            continue;
+        }
+
+        // 2.1 获取数字 / 连续数字 
         if (char >= '0' && char <= '9') {
             let numStr = '';
             while (i < str.length && str[i] >= '0' && str[i] <= '9') {
@@ -26,20 +33,24 @@ function lexer(str) {
             continue;
         }
 
-        //  3. 通过查找表匹配运算符 
-        for (let len = 3; len >= 1; len--) {
-            const candidate = str.slice(i, i + len);
-            if (operators.has(candidate)) {
-                tokens.push({ type: 'Operator', value: candidate});
-                i+= len;
-                continue whileLoop;
-            }
+        // 3.0 获取字母
+        if (char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z') {
+            tokens.push({type: 'Letter', value: char});
+            i++;
+            continue;
         }
 
-        tokens.push({ type: 'Symbol', value: char }) // 封装成 tokens 对象数组
-        i++;
-    }
+        // 3.1 获取连续字母
+        if (char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z') {
+            let word = '';
+            while (i < str.length && (str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' && str[i] <= 'Z')) {
+                word = + str[i];
+                i++;
+            }
+            tokens.push({ type: 'Word', value: word })
+        }
 
-    return tokens;
+        return tokens;
+    }
 }
 console.log(lexer('let n === 5 + 7'));
